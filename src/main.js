e4fef08d06139de58310db8e52fa880dbf2d20de
TreBraswell <49354031+TreBraswell@users.noreply.github.com>
2020-06-07 18:58:03 -0700

    small change to the info

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-/*Tre Braswell, Eric Suzuki, and Ixel Camacho
-Game Title: Yarny
-5-3-20
-Tre:Technically Interesting: I created a scaling feature that scaled the texture atlas depending on whether the player collected the button or a pin. The different sizes had different collision and velocity. I think this is technically interesting because i had to learn about scale and managing prefabs assets in between scenes.
-Ixel:Visual Style: Soft and charming theme with a hint of anime style to the character; the simple art captures adorableness, making you root for the yarn boy. Every art asset created is related to a color palette of dark and bright purple shades with the concept of sewing and yarn. The music, also, is created to make a childhood atmosphere with musical, fun sound effects. 
- 
-Few sounds were taken from online : jump sound https://freesound.org/people/Lefty_Studios/sounds/369515/, game over sound https://freesound.org/people/themusicalnomad/sounds/253886/, pageflip sound freesound.org
-*/let config = {
+let config = {
     type: Phaser.WEBGL,
     pixelArt: true,
     width: 840,
